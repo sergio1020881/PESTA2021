09e2ed53cef22e070304d4df43f1175bf031e80e
sergio1020881 <sergio.salazar.santos@gmail.com>
2021-03-22 17:26:16 +0000

    update_1

--- SandBox/ATMEGA128/Atmega128/Debug/makedep.mk
+++ SandBox/ATMEGA128/Atmega128/Debug/makedep.mk
@@ -2,17 +2,7 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-Libraria\74hc595.c
-
-Libraria\atmega128analog.c
-
-Libraria\atmega128i2c.c
-
 Libraria\atmega128timer.c
-
-Libraria\atmega128uart.c
-
-Libraria\clock.c
 
 Libraria\eeprom.c
 
@@ -20,21 +10,9 @@ Libraria\explode.c
 
 Libraria\function.c
 
-Libraria\keypad.c
-
 Libraria\lcd.c
 
-Libraria\lfsm.c
-
-Libraria\mm74c923.c
-
-Libraria\pcf8563rtc.c
-
-Libraria\rotenc.c
-
 Libraria\hx711.c
-
-Libraria\znpid.c
 
 main.c
 
